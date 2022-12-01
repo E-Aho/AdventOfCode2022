94ef9f3d6613a3b8ab294fc9ff0b5f050dabcba8
Erik Aho <erik.aho24@gmail.com>
2022-12-01 08:45:01 +0000

    Day 1 solution

--- src/Day01.py
+++ src/Day01.py
@@ -2,10 +2,28 @@ DAY_NUM = "01"
 
 
 def main(data):
-    pass
+    # Part 1
+    run_tots = [0]
+    max_val = 0
+    for val in data:
+        if val != '':
+            run_tots[-1] += int(val)
+        else:
+            if run_tots[-1] > max_val:
+                max_val = run_tots[-1]
+            run_tots.append(0)
+    print(f"Part 1 solution: {max_val}")
+
+    # Part 2:
+    # Could do more optimally with a heap for the top N during run
+    # But not at all limited by run time for this, below solution is fine
+
+    s = sorted(run_tots, reverse=True)
+    top_3 = sum(s[:3])
+    print(f"Part 2 solution: {top_3}")
 
 
 if __name__ == "__main__":
     with open(f"data/Day{DAY_NUM}.txt", "r") as file:
-        data = file.readlines()
+        data = [line.strip() for line in file.readlines()]
     main(data)
