0d6335b5e9ba069dfc3d1bef4b26b24c63891764
= <erik.aho24@gmail.com>
2022-12-03 22:24:45 +0000

    Complete Day 3

--- src/Day03.py
+++ src/Day03.py
@@ -1,12 +1,39 @@
 DAY_NUM = "03"
 
+def get_priority(char: str):
+    if char.isupper():
+        return ord(char) - 64 + 26   # ASCII code, A starts at 65
+    return ord(char) - 96      # ASCII code, a starts at 97
 
 def main(data):
     # Part 1
+    running_tot = 0
+    for rucksack in data:
+        comp_1, comp_2 = rucksack[:len(rucksack)//2], rucksack[len(rucksack)//2:]
+        shared_item = set(comp_1).intersection(set(comp_2)).pop()
+        running_tot += get_priority(shared_item)
+
+    print(f"Part 1: {running_tot}")
 
     # Part 2
-    pass
+    running_tot = 0
+    group = []
+    for rucksack in data:
+        group.append(rucksack)
 
+        if len(group) < 3:
+            continue
+
+        shared_item = (
+            set(group[0])
+            .intersection(set(group[1]))
+            .intersection(set(group[2]))
+            .pop()
+        )
+
+        running_tot += get_priority(shared_item)
+        group = []
+    print(f"Part 2: {running_tot}")
 
 
 if __name__ == "__main__":
