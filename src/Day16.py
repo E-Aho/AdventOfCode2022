2962697b782dcd9887d6bd72cde82184fdddbf89
= <erik.aho24@gmail.com>
2022-12-12 18:22:52 +0000

    Prep all days

---
+++ src/Day16.py
@@ -0,0 +1,11 @@
+DAY_NUM = "16"
+
+
+def main(data):
+    pass
+
+
+if __name__ == "__main__":
+    with open(f"data/Day{DAY_NUM}.txt", "r") as file:
+        cleaned_data = [line.strip() for line in file.readlines()]
+    main(cleaned_data)
