894ffda8ca49897b8ce7ef178d10787af4639583
= <erik.aho24@gmail.com>
2022-12-12 18:24:11 +0000

    Update days

--- src/Day21.py
+++ src/Day21.py
@@ -2,10 +2,10 @@ DAY_NUM = "21"
 
 
 def main(data):
-    pass
+    print(data)
 
 
 if __name__ == "__main__":
     with open(f"data/Day{DAY_NUM}.txt", "r") as file:
-        cleaned_data = [line.strip() for line in file.readlines()]
+        cleaned_data = [line.strip().replace("\n", "") for line in file.readlines()]
     main(cleaned_data)
